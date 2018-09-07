10aad2cdfd9cbca01bbf33334bbeac6da98bd764
jinxinrui <rxjinxinrui@gmail.com>
2018-09-07 14:07:33 +1000

    References added, subtitle of header changed

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'If you are 40 to 60 years old, find out how you can reduce the change of getting Alzheimer\'s in future.';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
